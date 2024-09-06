337cbaf5a31f736f1e74c2b8b29326590fd788d4
Valery <Valery@Egors-MacBook-Pro-2.local>
2024-09-06 10:07:48 +0300

    1-начальный соммит пустой проект

---
+++ TableView_Teach_GitHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView_Teach_GitHub
+//
+//  Created by Valery on 06.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
